858b0fa1e4631e20d0f15affb9b681a90c4d9cd0
david <davidndy@gmail.com>
2017-11-09 18:16:41 -0500

    Re intentando el ejercicio 2

--- solution.rb
+++ solution.rb
@@ -1,22 +1,7 @@
-# pseudo codigo.
-# 1.recibir el request con un metodo get proveniente de la direccion '/'
-# 2. presentar la pagina con el valor del contador y el boton
-# 3. cuando se le de click al boton ir a la misma pagina
-# 4. en el metodo get recibir el valor en params
-# 5. aumentar la cuenta en el archivo de views 
-# 6. subir la pagina con el nuevo valor del contador.
-# 	
-
-
 require 'sinatra'
 require 'pry'
 
-get '/' do
-erb :mijito
-end 
-
-post '/abuelita' do	
-	@mensaje = params[:mensaje]
-	binding.pry
-	erb :abuelita
+get '/makers/:name' do
+  	"<h1>Hola #{params[:name].capitalize}!</h1>"
 end
+#----
